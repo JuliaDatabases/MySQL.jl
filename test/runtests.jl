f72a949eef852949c982203fe1381d23030fbb65
nkottary <kottarynishanth54@gmail.com>
2015-09-21 12:41:44 +0530

    Added .travis.yml

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Base.Test
 
 # Change the below values to whatever it is on your test setup.
 const HOST = "127.0.0.1"
-const ROOTPASS = "root"
+const ROOTPASS = "" # In Travis CI the root password is an empty string.
 
 for file in ["test_basic.jl", ]
     include(file)
