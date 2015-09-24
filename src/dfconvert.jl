ffdd3775fadaaf5d1b2289723cb6d9962824f408
nkottary <kottarynishanth54@gmail.com>
2015-09-24 16:16:20 +0530

    Refactor dfconvert.jl

--- src/dfconvert.jl
+++ src/dfconvert.jl
@@ -55,7 +55,7 @@ end
 """
 Fill the row indexed by `row` of the dataframe `df` with values from `result`.
 """
-function populate_row(numFields::Int8, mysqlfield_types::Array{Uint32}, result::MySQL.MYSQL_ROW, df, row)
+function populate_row!(df, numFields::Int8, mysqlfield_types::Array{Uint32}, result::MySQL.MYSQL_ROW, row)
     for i = 1:numFields
         value = ""
         obj = unsafe_load(result.values, i)
@@ -149,31 +149,35 @@ function results_to_dataframe(results::MYSQL_RES)
 
     for row = 1:n_rows
         result = MySQL.mysql_fetch_row(results)
-        populate_row(n_fields, mysqlfield_types, result, df, row)
+        populate_row!(df, n_fields, mysqlfield_types, result, row)
         result = null
     end
 
     return df
 end
 
-function stmt_populate_row(n_fields::Int8, mysqlfield_types::Array{Uint32}, df, row, juBindArray)
+"""
+Populate a row in the dataframe `df` indexed by `row` given the number of fields `n_fields`,
+ the type of each field `mysqlfield_types` and an array `jbindarr` to which the results are bound.
+"""
+function stmt_populate_row!(df, n_fields::Int8, mysqlfield_types::Array{Uint32}, row, jbindarr)
     for i = 1:n_fields
         value = ""
         if (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_TINY ||
             mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_ENUM ||
             mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_BIT)
-            @compat value = parse(Int8, juBindArray[i].buffer_int[1])
+            @compat value = parse(Int8, jbindarr[i].buffer_int[1])
         elseif (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_SHORT)
-            @compat value = parse(Int16, juBindArray[i].buffer_int[1])
+            @compat value = parse(Int16, jbindarr[i].buffer_int[1])
         elseif (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_LONG ||
                 mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_INT24)
-            @compat value = parse(Int32, juBindArray[i].buffer_int[1])
+            @compat value = parse(Int32, jbindarr[i].buffer_int[1])
         elseif (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_LONGLONG)
-            @compat value = parse(Int64, juBindArray[i].buffer_long[1])
+            @compat value = parse(Int64, jbindarr[i].buffer_long[1])
         elseif (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_DECIMAL ||
                 mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_NEWDECIMAL)
             ### Not supported fully !!! this may work in some cases
-            data  = juBindArray[i].buffer_string
+            data  = jbindarr[i].buffer_string
             value = 0.0
             if (!isempty(data))
                 idx  = findfirst(data, '\0')
@@ -185,11 +189,11 @@ function stmt_populate_row(n_fields::Int8, mysqlfield_types::Array{Uint32}, df, 
         elseif (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_FLOAT ||
                 mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_DOUBLE)
             value = 0.0
-            if (juBindArray[i].buffer_double[1] != C_NULL)
-                @compat value = parse(Float64, juBindArray[i].buffer_double[1])
+            if (jbindarr[i].buffer_double[1] != C_NULL)
+                @compat value = parse(Float64, jbindarr[i].buffer_double[1])
             end    
         elseif (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_DATETIME)
-            mysql_time = juBindArray[i].buffer_datetime[1]
+            mysql_time = jbindarr[i].buffer_datetime[1]
             if (mysql_time.year != 0)   ## to handle invalid data like 0000-00-00T00:00:00
                 value = DateTime(mysql_time.year, mysql_time.month, mysql_time.day,
                                  mysql_time.hour, mysql_time.minute, mysql_time.second)
@@ -197,14 +201,14 @@ function stmt_populate_row(n_fields::Int8, mysqlfield_types::Array{Uint32}, df, 
                 value = DateTime()
             end
         elseif (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_DATE)
-            mysql_date = juBindArray[i].buffer_date[1]
+            mysql_date = jbindarr[i].buffer_date[1]
             if (mysql_date.year != 0)   ## to handle invalid data like 0000-00-00
                 value = Date(mysql_date.year, mysql_date.month, mysql_date.day)
             else
                 value = Date()
             end
         else
-            data  = juBindArray[i].buffer_string
+            data  = jbindarr[i].buffer_string
             if (!isempty(data))
                 idx  = findfirst(data, '\0')
                 value = bytestring(data[1:(idx == 0 ? endof(data) : idx-1)])
@@ -214,6 +218,9 @@ function stmt_populate_row(n_fields::Int8, mysqlfield_types::Array{Uint32}, df, 
     end    
 end
 
+"""
+Convert the MySQL results `results` to a DataFrame given a prepared statement `stmtptr`.
+"""
 function stmt_results_to_dataframe(results::Ptr{Cuchar}, stmtptr::Ptr{Cuchar}=C_NULL)
     n_fields = MySQL.mysql_num_fields(results)
     fields = MySQL.mysql_fetch_fields(results)
@@ -224,15 +231,15 @@ function stmt_results_to_dataframe(results::Ptr{Cuchar}, stmtptr::Ptr{Cuchar}=C_
     jfield_types = Array(DataType, n_fields)
     field_headers = Array(Symbol, n_fields)
     mysqlfield_types = Array(Uint32, n_fields)
-    bindArray = null
-    juBindArray = null
+    mysql_bindarr = null
+    jbindarr = null
     
-    bindArray = MySQL.MYSQL_BIND[]
-    juBindArray = MySQL.JU_MYSQL_BIND[]
+    mysql_bindarr = MySQL.MYSQL_BIND[]
+    jbindarr = MySQL.JU_MYSQL_BIND[]
     
     for i = 1:n_fields
         mysql_field = unsafe_load(fields, i)
-        jfield_types[i] = Array(mysql_to_julia_type(mysql_field.field_type), n_rows)
+        jfield_types[i] = mysql_to_julia_type(mysql_field.field_type)
         field_headers[i] = symbol(bytestring(mysql_field.name))
         mysqlfield_types[i] = mysql_field.field_type
         field_length = mysql_field.field_length
@@ -246,7 +253,7 @@ function stmt_results_to_dataframe(results::Ptr{Cuchar}, stmtptr::Ptr{Cuchar}=C_
         my_buff_double = Array(Cdouble)
         my_buff_string = Array(Uint8, field_length)
         my_buff_datetime = Array(MySQL.MYSQL_TIME)
-        my_buff
+        my_buff = null
         
         if (mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_LONGLONG ||
             mysqlfield_types[i] == MySQL.MYSQL_TYPES.MYSQL_TYPE_INT24)
@@ -309,31 +316,31 @@ function stmt_results_to_dataframe(results::Ptr{Cuchar}, stmtptr::Ptr{Cuchar}=C_
             my_buff = my_buff_string
         end
         
-        bind = MySQL.MYSQL_BIND(buffer_type, pointer(tmp_long), pointer(tmp_char),
-                reinterpret(Ptr{Void}, pointer(my_buff)), buffer_length)
+        bind = MySQL.MYSQL_BIND(pointer(tmp_long), pointer(tmp_char),
+                                reinterpret(Ptr{Void}, pointer(my_buff)),
+                                buffer_length, buffer_type)
         juBind = MySQL.JU_MYSQL_BIND(tmp_long, tmp_char, my_buff_long, my_buff_int,
-                                        my_buff_double, my_buff_string, my_buff_datetime)
-        push!(bindArray, bind)
-        push!(juBindArray, juBind)
+                                     my_buff_double, my_buff_string, my_buff_datetime)
+        push!(mysql_bindarr, bind)
+        push!(jbindarr, juBind)
         mysql_field = null
     end
     
     df = DataFrame(jfield_types, field_headers, n_rows)
     response = MySQL.mysql_stmt_bind_result(stmtptr, reinterpret(Ptr{Cuchar},
-                                            pointer(bindArray)))
+                                            pointer(mysql_bindarr)))
     if (response != 0)
         println("the error after bind result is ::: $(bytestring(MySQL.mysql_stmt_error(stmtptr)))")
         return df
     end
 
     for row = 1:n_rows
-        result = null
         result = MySQL.mysql_stmt_fetch_row(stmtptr)
-        if (result != 0)
+        if (result == C_NULL)
             println("Could not fetch row ::: $(bytestring(MySQL.mysql_stmt_error(stmtptr)))")
             return df
         else
-            stmt_populate_row(n_fields, mysqlfield_types, df, row, juBindArray)
+            stmt_populate_row!(df, n_fields, mysqlfield_types, row, jbindarr)
         end
     end
     return df
