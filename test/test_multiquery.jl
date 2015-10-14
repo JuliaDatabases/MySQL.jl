6c08c9356d065580f3a2629df8bc14e3308d1de7
nkottary <kottarynishanth54@gmail.com>
2015-10-14 22:03:10 +0530

    Fix travis test issue

--- test/test_multiquery.jl
+++ test/test_multiquery.jl
@@ -6,13 +6,15 @@ function run_test()
     println("*** Running multi query test ***\n")
     connect_as_root()
 
-    query = """use some_db;
+    query = """CREATE DATABASE test_db;
+    USE test_db;
     DROP TABLE IF EXISTS test_table;
     CREATE TABLE test_table(id INT);
     INSERT INTO test_table VALUES(10);
     UPDATE test_table SET id=20 WHERE id=10;
     SELECT * FROM test_table;
-    DROP TABLE test_table"""
+    DROP TABLE test_table
+    DROP DATABASE test_db"""
 
     mysql_execute_multi_query(con, query)
     affrows, data = mysql_execute_multi_query(con, query)    # Run twice.
