3e27d1e9d1fb9a0f5ba72501c24f1a470efc56b4
nkottary <kottarynishanth54@gmail.com>
2015-10-16 13:34:46 +0530

    Refactor Benchmark code

--- test/Benchmarks/benchmark.jl
+++ test/Benchmarks/benchmark.jl
@@ -1,57 +1,111 @@
+# A performance test, since we are using Faker this only works on v0.4
+
 using MySQL
 using DataFrames
-using JLD, HDF5
+
 include("create_datasets.jl")
-number_of_datasets = 10000
-Multi_Query = 0
 
-function mysql_benchmarks(queries,use_prepare=0, Multi_Query = 0,operation=" ")
-  conn = mysql_connect("127.0.0.1", "root", "root", "mysqltest1")
-  println("Time taken by MySQL wrapper for operation $operation on a dataset with 100000 rows and 12 coloumns (Composed of 12 different datatypes) is as follows")
-  if use_prepare ==1
-    stmt = mysql_stmt_init(conn)
-    @time for i = 1:size(queries,1)
-      mysql_stmt_prepare(stmt, queries[i])
-      mysql_stmt_execute(stmt)
-    end
-    mysql_stmt_close(stmt)
-  elseif Multi_Query ==1
-      temp = join(queries)
-      @time mysql_execute_query(conn, temp)
-  else
-    @time for i = 1:size(queries,1)
-      mysql_execute_query(conn, queries[i])
-    end
-  end
-  println("Time taken for retrieving all the Inserted or Updated records by MySQL wrapper is ")
-  @time mysql_execute_query(conn, "select ID, Name, Salary, OfficeNo, JobType,h, n, z, z1, z2, cha, empno from Employee")
-  mysql_disconnect(conn)
-end
+const NUMDATASETS = 10000
+const HOST = "127.0.0.1"
+const USER = "root"
+const ROOTPASS = ""
+const DBNAME = ""
 
-function delete_table()
-    conn = mysql_connect("127.0.0.1", "root", "root", "mysqltest1")
-    try
-      mysql_execute_query(conn,"drop table Employee")
-      mysql_disconnect(conn)
-    catch
-      mysql_disconnect(conn)
-      return
+function mysql_benchmarks(queries; use_prepare=0, use_multiquery = 0, operation=" ")
+    println("\n*** Operation $operation Time:\n")
+    if use_prepare == 1
+        println("*** Prepare statement test ")
+        stmt = mysql_stmt_init(conn)
+        @time for i = 1:size(queries, 1)
+            mysql_stmt_prepare(stmt, queries[i])
+            mysql_stmt_execute(stmt)
+        end
+        mysql_stmt_close(stmt)
+    elseif use_multiquery == 1
+        println("*** Multi query test ")
+        temp = join(queries)
+        @time mysql_execute_multi_query(conn, temp)
+    else
+        println("*** Normal test ")
+        @time for i = 1:size(queries, 1)
+            mysql_execute_query(conn, queries[i])
+        end
     end
 end
-function run_all_benchmarks()
-  delete_table()
-  println("Benchmark without using Prepare functionality")
-  mysql_benchmarks(insert_queries(number_of_datasets),0,0,"Insert")
-  mysql_benchmarks(update_queries(number_of_datasets),0,0,"Update")
-  println("Benchmark using Prepare functionality")
-  delete_table()
-  mysql_benchmarks(insert_queries(number_of_datasets),1,0,"Insert")
-  mysql_benchmarks(update_queries(number_of_datasets),1,0,"Update")
-  println("Benchmark using Multiquery functionality")
-  delete_table()
-  mysql_benchmarks(insert_queries(number_of_datasets),0,1,"Insert")
-  mysql_benchmarks(update_queries(number_of_datasets),0,1,"Update")
+
+function init_test()
+    println("*** Initializing test")
+    global conn = mysql_connect(HOST, USER, ROOTPASS, DBNAME)
+    if conn == C_NULL
+        error("mysql_connect failed")
+    end
+
+    # Cleanup leftovers from previous test and create table.
+    query = """DROP DATABASE IF EXISTS mysqltest;
+    CREATE DATABASE mysqltest;
+    USE mysqltest;
+    CREATE TABLE Employee(
+        ID INT NOT NULL AUTO_INCREMENT,
+        Name VARCHAR(4000),
+        Salary FLOAT,
+        LastLogin DATETIME,
+        OfficeNo TINYINT,
+        JobType ENUM('HR', 'Management', 'Accounts'),
+        h MEDIUMINT,
+        n INTEGER,
+        z BIGINT,
+        z1 DOUBLE,
+        z2 DOUBLE PRECISION,
+        cha CHAR,
+        empno SMALLINT,
+        PRIMARY KEY (ID));"""
+
+    mysql_execute_multi_query(conn, query)
+    println("*** Done initializing test.")
 end
 
-number_of_datasets = number_of_datasets + 1
-run_all_benchmarks()
+function cleanup_test()
+    println("*** Cleaning up")
+    response = mysql_query(conn, "DROP DATABASE mysqltest;")
+    mysql_display_error(conn, response, "Cleanup failed.")
+    mysql_disconnect(conn)
+    println("*** Done cleanup")
+end
+
+function retrieve_test()
+    @time mysql_execute_query(conn, "select ID, Name, Salary, OfficeNo, JobType, h, n, z, z1, z2, cha, empno from Employee")
+end
+
+function run_test(num_datasets)
+    init_test()
+
+    println("Benchmark without using Prepare functionality")
+    mysql_benchmarks(insert_queries(num_datasets),
+                     use_prepare=0, use_multiquery=0, operation="Insert")
+    mysql_benchmarks(update_queries(num_datasets),
+                     use_prepare=0, use_multiquery=0, operation="Update")
+    retrieve_test()
+
+    println("Benchmark using Prepare functionality")
+    mysql_benchmarks(insert_queries(num_datasets),
+                     use_prepare=1, use_multiquery=0, operation="Insert")
+    mysql_benchmarks(update_queries(num_datasets),
+                     use_prepare=1, use_multiquery=0, operation="Update")
+    retrieve_test()
+
+    println("Benchmark using Multiquery functionality")
+    mysql_benchmarks(insert_queries(num_datasets),
+                     use_prepare=0, use_multiquery=1, operation="Insert")
+    mysql_benchmarks(update_queries(num_datasets),
+                     use_prepare=0, use_multiquery=1, operation="Update")
+    retrieve_test()
+
+    cleanup_test()
+end
+
+function create_large_table(num_datasets)
+    init_test()
+
+    mysql_benchmarks(insert_queries(num_datasets),
+                     use_prepare=0, use_multiquery=0, operation="Insert")
+end
--- test/Benchmarks/create_datasets.jl
+++ test/Benchmarks/create_datasets.jl
@@ -1,54 +1,45 @@
 using Faker
-using JLD, HDF5
-number_of_datasets = 100000
 
-function create_queries(number_of_datasets=100001)
-  # TINYINT
-  tint = [rand(-128:127) for i=1 : number_of_datasets]
-  # SMALLINT
-  sint = [rand(-32768:32767) for i=1 : number_of_datasets]
-  # MEDIUMINT
-  mint = [rand(-8388608:8388607) for i=1 : number_of_datasets]
-  # INT and INTEGER
-  rint = [rand(-2147483648:2147483647) for i=1 : number_of_datasets]
-  # BIGINT
-  bint = [rand(-9223372036854775808:9223372036854775807) for i=1 : number_of_datasets]
-  # FLOAT
-  rfloat = [rand(Float16) for i=1 : number_of_datasets]
-  # DOUBLE
-  dfloat = [rand(Float32) for i=1 : number_of_datasets]
-  # DOULBE PRECISION, REAL, DECIMAL
-  dpfloat = [rand(Float64) for i=1 : number_of_datasets]
-  #DATETIME, TIMESTAMP, DATE
-  datetime = [Faker.date_time_ad() for i=1 : number_of_datasets]
-  #CHAR
-  chara = [randstring(1) for i=1 : number_of_datasets]
-  #VARCHAR , VARCHAR2, TINYTEXT, TEXT
-  varcha = [randstring(rand(1:4000)) for i=1 : number_of_datasets]
-  #ENUM
-  JobType = ["HR", "Management", "Accounts"]
-  enume = [JobType[rand(1:3)] for i=1 : number_of_datasets]
-  return varcha, rfloat, datetime, tint, enume, mint, rint, bint, dfloat, dpfloat, chara, sint
+function create_queries(number_of_datasets)
+    println("*** Generating random data for $number_of_datasets tuples")
+    tint = [rand(-128:127) for i=1 : number_of_datasets]                                     # TINYINT
+    sint = [rand(-32768:32767) for i=1 : number_of_datasets]                                 # SMALLINT
+    mint = [rand(-8388608:8388607) for i=1 : number_of_datasets]                             # MEDIUMINT
+    rint = [rand(-2147483648:2147483647) for i=1 : number_of_datasets]                       # INT and INTEGER
+    bint = [rand(-9223372036854775808:9223372036854775807) for i=1 : number_of_datasets]     # BIGINT
+    rfloat = [rand(Float16) for i=1 : number_of_datasets]                                    # FLOAT
+    dfloat = [rand(Float32) for i=1 : number_of_datasets]                                    # DOUBLE
+    dpfloat = [rand(Float64) for i=1 : number_of_datasets]                                   # DOULBE PRECISION, REAL, DECIMAL
+    datetime = [Faker.date_time_ad() for i=1 : number_of_datasets]                           # DATETIME, TIMESTAMP, DATE
+    chara = [randstring(1) for i=1 : number_of_datasets]                                     # CHAR
+    varcha = [randstring(rand(1:4000)) for i=1 : number_of_datasets]                         # VARCHAR , VARCHAR2, TINYTEXT, TEXT
+    jobtype = ["HR", "Management", "Accounts"]                                               # ENUM
+    enume = [jobtype[rand(1:3)] for i=1 : number_of_datasets]
+
+    println("*** Done generating random data")
+    return varcha, rfloat, datetime, tint, enume, mint, rint, bint, dfloat, dpfloat, chara, sint
 end
 
-function insert_queries(number_of_datasets=100001)
-    Insert_Query = Array(String, (number_of_datasets))
-    create_table = "CREATE TABLE Employee(ID INT NOT NULL AUTO_INCREMENT, Name VARCHAR(4000), Salary FLOAT, LastLogin DATETIME, OfficeNo TINYINT, JobType ENUM('HR', 'Management', 'Accounts'), h MEDIUMINT, n INTEGER, z BIGINT, z1 DOUBLE, z2 DOUBLE PRECISION, cha CHAR, empno SMALLINT, PRIMARY KEY (ID));"
-    Insert_Query[1] = create_table
+function insert_queries(number_of_datasets)
+    println("*** Generating insert queries for $number_of_datasets tuples")
+    insert_query = Array(String, number_of_datasets)
     varcha, rfloat, datetime, tint, enume, mint, rint, bint, dfloat, dpfloat, chara, sint = create_queries(number_of_datasets)
-    for i=2 :(number_of_datasets)
-      Insert_Query[i] = "INSERT INTO Employee (Name, Salary, LastLogin, OfficeNo, JobType,h, n, z, z1, z2, cha, empno) VALUES ('$(varcha[i])', $(rfloat[i]), '$(datetime[i])', $(tint[i]), '$(enume[i])', $(mint[i]), $(rint[i]), $(bint[i]), $(dfloat[i]), $(dpfloat[i]), '$(chara[i])', $(sint[i]));"
+    for i = 1:number_of_datasets
+        insert_query[i] = "INSERT INTO Employee (Name, Salary, LastLogin, OfficeNo, JobType,h, n, z, z1, z2, cha, empno) VALUES ('$(varcha[i])', $(rfloat[i]), '$(datetime[i])', $(tint[i]), '$(enume[i])', $(mint[i]), $(rint[i]), $(bint[i]), $(dfloat[i]), $(dpfloat[i]), '$(chara[i])', $(sint[i]));"
     end
-    return Insert_Query
+    println("*** Done generating insert queries")
+    return insert_query
 end
 
 
-function update_queries(number_of_datasets=100001)
-    number_of_datasets = number_of_datasets -1
-    Update_Query = Array(String, (number_of_datasets))
+function update_queries(number_of_datasets)
+    println("*** Generating update queries for $number_of_datasets tuples")
+    number_of_datasets = number_of_datasets
+    update_query = Array(String, number_of_datasets)
     varcha, rfloat, datetime, tint, enume, mint, rint, bint, dfloat, dpfloat, chara, sint = create_queries(number_of_datasets)
-    for i=1 : (number_of_datasets)
-      Update_Query[i] = "UPDATE Employee SET Name='$(varcha[i])', Salary=$(rfloat[i]), LastLogin='$(datetime[i])', OfficeNo=$(tint[i]), JobType='$(enume[i])', h=$(mint[i]), n=$(rint[i]), z=$(bint[i]), z1=$(dfloat[i]), z2=$(dpfloat[i]), cha='$(chara[i])', empno=$(sint[i]) where ID = $i;"
+    for i = 1:number_of_datasets
+        update_query[i] = "UPDATE Employee SET Name='$(varcha[i])', Salary=$(rfloat[i]), LastLogin='$(datetime[i])', OfficeNo=$(tint[i]), JobType='$(enume[i])', h=$(mint[i]), n=$(rint[i]), z=$(bint[i]), z1=$(dfloat[i]), z2=$(dpfloat[i]), cha='$(chara[i])', empno=$(sint[i]) where ID = $i;"
     end
-    return Update_Query
+    println("*** Done generating update queries")
+    return update_query
 end
