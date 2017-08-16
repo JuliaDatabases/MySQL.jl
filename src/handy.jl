94cd8234346038e6ce4e513341ebc440701a1c24
nkottary <kottarynishanth54@gmail.com>
2017-08-16 12:57:33 +0530

    Wrap number of affected rows

--- src/handy.jl
+++ src/handy.jl
@@ -146,8 +146,10 @@ function mysql_execute(hndl, command; opformat=MYSQL_DATA_FRAME)
 
     if opformat == MYSQL_DATA_FRAME
         convfunc = mysql_result_to_dataframe
+        narfunc = n -> DataFrame(num_affected_rows=[n])
     elseif opformat == MYSQL_TUPLES
         convfunc = mysql_get_result_as_tuples
+        narfunc = n -> (n, )
     else
         throw(MySQLInterfaceError("Invalid output format: $opformat"))
     end
@@ -159,7 +161,8 @@ function mysql_execute(hndl, command; opformat=MYSQL_DATA_FRAME)
             push!(data, retval)
 
         elseif mysql_field_count(hndl.mysqlptr) == 0
-            push!(data, Int(mysql_affected_rows(hndl.mysqlptr)))
+            n = Int(mysql_affected_rows(hndl.mysqlptr))
+            push!(data, narfunc(n))
         else
             throw(MySQLInterfaceError("Query expected to produce results but did not."))
         end
