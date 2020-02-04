5de6623971ddca95a49c9d7d9ef9cf3fa850f06b
Jacob Quinn <quinn.jacobd@gmail.com>
2020-02-03 20:27:47 -0700

    Update BinaryProvider dep

--- deps/build.jl
+++ deps/build.jl
@@ -4,7 +4,7 @@ using BinaryProvider # requires BinaryProvider 0.3.0 or later
 
 # Parse some basic command-line arguments
 const verbose = "--verbose" in ARGS
-const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "lib")))
+const prefix = Prefix(get([a for a in ARGS if a != "--verbose"], 1, joinpath(@__DIR__, "lib/mariadb")))
 products = [
     LibraryProduct(prefix, "libmariadb", :libmariadb),
 ]
