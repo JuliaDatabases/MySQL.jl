b604e6bfa6b9c993555a34232f58182e25243e1e
Jacob Quinn <quinn.jacobd@gmail.com>
2022-10-04 22:08:51 -0600

    Updates for 1.8

--- src/api/API.jl
+++ src/api/API.jl
@@ -4,22 +4,8 @@ using Dates, DecFP
 
 export DateAndTime
 
-if VERSION < v"1.3.0"
-
-# Load libmariadb from our deps.jl
-const depsjl_path = joinpath(dirname(@__FILE__), "..", "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("MySQL not installed properly, run Pkg.build(\"MySQL\"), restart Julia and try again")
-end
-include(depsjl_path)
-const PLUGIN_DIR = joinpath(dirname(@__FILE__), "..", "..", "deps", "usr", "lib", "mariadb", "plugin")
-
-else
-
 using MariaDB_Connector_C_jll
 const PLUGIN_DIR = joinpath(MariaDB_Connector_C_jll.artifact_dir, "lib", "mariadb", "plugin")
-
-end
 
 # const definitions from mysql client library
 include("consts.jl")
